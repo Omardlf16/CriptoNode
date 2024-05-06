5bef256b90bc15660f152c5b265d424053ece693
Omar Diaz de Leon Flores <odiaz@frioexpress.com>
2024-05-05 19:37:31 -0600

    Se agrego archivo faltante

---
+++ diffie-hellman/index.js
@@ -0,0 +1,52 @@
+const crypto = require('crypto');
+
+function diffieHellman ( encoding, from=false ) {
+
+    // {
+    //   prime,
+    //   generator,
+    //   publicKey,
+    //   privateKey
+    // }
+
+    if ( !from ) {
+        const dh = crypto.createDiffieHellmanGroup("modp14");
+        const publicKey = dh.generateKeys();
+
+        return {
+            prime: dh.getPrime().toString(encoding),
+            generator: dh.getGenerator().toString(encoding),
+            publicKey: publicKey.toString(encoding),
+            privateKey: dh.getPrivateKey().toString(encoding), // No share!!
+          };
+
+    } else {
+        console.log(from)
+        const dh = crypto.createDiffieHellman(
+            from.prime,
+            encoding,
+            from.generator,
+            encoding
+        );
+
+        dh.setPrivateKey(from.privateKey, encoding);
+        dh.setPublicKey(from.publicKey, encoding);
+    
+        const secret = dh.computeSecret(from.publicKey, encoding);
+    
+        return {
+          prime: dh.getPrime().toString(encoding),
+          generator: dh.getGenerator().toString(encoding),
+          publicKey: dh.getPublicKey().toString(encoding),
+          privateKey: dh.getPrivateKey().toString(encoding), // No share!!
+          secret: secret.toString(encoding)
+        };
+
+        return 'Aqui se combinas las claves y se acuerda la key en conjunto';
+    }
+  
+}
+
+module.exports = {
+    "diffieHellman": diffieHellman
+}
